8f45c49a37d3a6972bc32f6813ea222ee9fd68be
zeftambornino@gmail.com <zeftambornino@gmail.com>
2019-12-28 22:06:14 -0600

    finished css and added DOM elements

--- logic.js
+++ logic.js
@@ -1,4 +1,10 @@
 function myFunction() {
-    var element = document.getElementById("spinner");
+
+var element = document.getElementById("spinner");
     element.classList.add("spinning");
+    if(element.classList.contains(spin)) {
+        element.classList.remove(spin);
+    } else {
+        element.classList.add(spin);    
+    }
 }
